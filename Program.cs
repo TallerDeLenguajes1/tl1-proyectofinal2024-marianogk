1757d063d7681bd32f63e34363e6619f4321f153
marianogk <marianokpo7@gmail.com>
2024-07-17 21:14:37 -0300

    la pelea empieza bien, pero la salud se vuelve negativa

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ public class Programa
 
     public static async Task Main(string[] args)
     {
+        // Lista de id de los personajes de la api
+        List<int> idsPersonajes = new List<int> { 332, 659, 620, 623, 579, 165, 344, 720, 107, 69, 309 };
+
         string archivoPersonajes = @"C:\taller1\tl1-proyectofinal2024-marianogk\characters.json";
 
         // Crear 10 personajes si no existe el archivo
@@ -19,12 +22,11 @@ public class Programa
             int p = 0;
             while (personajes.Count < 11)
             {
-                List<int> idsPersonajes = new List<int> { 332, 659, 620, 623, 579, 165, 344, 720, 107, 69, 309 };
-
-                int idP = idsPersonajes[p];
+                
+                int id = idsPersonajes[p];
                 p++;
 
-                Root salida = await Api.GetPersonaje(idP); // Obtener el personaje desde la API                
+                Root salida = await Api.GetPersonaje(id); // Obtener el personaje desde la API                
 
                 if (salida != null)
                 {
@@ -56,26 +58,47 @@ public class Programa
             {
                 Console.WriteLine("No se encontraron personajes en el archivo JSON.");
             }
+
+
+
+            // Elegir 2 personajes para la pelea
+
+            // Crear un indice aleatorio para los 2 personajes
+            Random randomId = new Random();
+
+            int idP1 = randomId.Next(0, idsPersonajes.Count);
+            int idP2 = randomId.Next(0, idsPersonajes.Count);
+
+            List<Personaje> personajesElegidos = new List<Personaje>();
+
+            Personaje player1, player2;
+
+            player1 = personajesLeidos[idP1];
+
+            player2 = personajesLeidos[idP2];
+
+            personajesElegidos.Add(player1);
+            personajesElegidos.Add(player2);
+
+            PersonajesJson.MostrarPersonajes(personajesElegidos);
+
+            // Pelear
+
+            Personaje ganador;
+
+            ganador = Batalla.Pelear(player1, player2);
+
+            // Mostrar y guardar ganador
+
+            Console.WriteLine("El ganador es: " + ganador);
+
+            string archivoHistorial = @"C:\taller1\tl1-proyectofinal2024-marianogk\historial.json";
+
+            await HistorialJson.GuardarGanador(ganador, archivoHistorial);
+            Console.WriteLine("Ganador guardado en el archivo de historial correctamente.");
         }
 
-        // Elegir 2 personajes para la pelea
 
-
-
-        // Pelear
-
-        Personaje ganador = new Personaje();
-
-        ganador = Batalla.Pelear(player1, player2);
-
-        // Mostrar y guardar ganador
-
-        Console.WriteLine("El ganador es: " + ganador);
-
-        string archivoHistorial = @"C:\taller1\tl1-proyectofinal2024-marianogk\historial.json";
-
-        await HistorialJson.GuardarGanador(ganador, archivoHistorial);
-        Console.WriteLine("Ganador guardado en el archivo de historial correctamente.");
 
     }
 
