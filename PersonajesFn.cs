d178818ad4f126b48a8881c473ae02bbe59730c3
marianogk <marianokpo7@gmail.com>
2024-08-09 01:56:09 -0300

    nuevo archivo para funciones de los personajes

---
+++ PersonajesFn.cs
@@ -0,0 +1,12 @@
+using EspacioPersonaje;
+
+public static class PersonajesFn
+{
+    public static float GetSaludInicial(Personaje pj)
+    {
+        return pj.Caracteristicas.Salud;
+    }
+
+
+
+}
